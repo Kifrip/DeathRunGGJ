2930a616b5c87f6496b3ca0c2423115188548356
Kifrip <kifrip@gmail.com>
2024-01-28 14:03:49 +0200

    Added Audio

---
+++ Assets/Audio/AudioEnums - JSAM_library.cs
@@ -0,0 +1,26 @@
+    public enum JSAM_librarySounds {
+        death_1_sean,
+        death_2_sean,
+        death_3_sean,
+        death_4_sean,
+        death_5_sean,
+        death_6_sean,
+        death_7_sean,
+        death_8_sean,
+        death_9_sean,
+        death_10_sean,
+        joke_1,
+        joke_2,
+        joke_3,
+        joke_4,
+        joke_5,
+        joke_6,
+        joke_7,
+        joke_8,
+        joke_9,
+        joke_10,
+        allJokes
+    }
+    public enum JSAM_libraryMusic {
+        OwU
+    }
